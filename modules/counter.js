dd924d7473f27dee21b96e4855935100ec03d4d7
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-20 00:33:42 +0900

    Ducks 패턴

---
+++ modules/counter.js
@@ -0,0 +1,35 @@
+/**
+ * Ducks pattern
+ * 한곳에 만든다 -> 액션 타입, 액션 생성함수, 리듀서 함수 => "모듈"
+ */
+
+// 액션타입
+const INCREASE = "counter/INCREASE"; // 보통네이밍을 "모듈이름/액션이름" 으로 한다
+const DECREASE = "counter/DECREASE";
+
+// 액션생성 함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 초기값
+const initialState = {
+  number: 0,
+};
+
+// 리듀서 함수, reducer function
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
---
+++ modules/todos.js
@@ -0,0 +1,80 @@
+// 액션 타입
+const CHANGE_INPUT = "todos/CHANGE_INPUT"; // 인풋값을 변경함
+const INSERT = "todos/INSERT"; // 새로운 todo 를 등록함
+const TOGGLE = "todos/TOGGLE"; // todo 를 체크/헤제
+const REMOVE = "todos/REMOVE"; // todo 를 제거
+
+// 액션 생성함수 ( 파라미터 포함 )
+export const changeInput = (input) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+let id = 3; // 초기값
+export const insert = (text) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
+
+// 초기값
+const initialState = {
+  input: "",
+  todos: [
+    {
+      id: 1,
+      text: "리덕스1",
+      done: true,
+    },
+    {
+      id: 2,
+      text: "리덕스2",
+      done: false,
+    },
+  ],
+};
+
+// 리듀서 함수 ( 불변성 유지를 위한 spread 연산자 사용 )
+function todos(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input,
+      };
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo),
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo
+        ),
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default todos;
